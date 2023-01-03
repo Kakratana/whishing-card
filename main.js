d9ef1e6038a0c1e922c7555b9853f6886eeb1753
Kakratana <39192742+Kakratana@users.noreply.github.com>
2023-01-03 14:34:46 +0700

    cc

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function prev(){
       // ctx.fillRect(0, 750, 800, 50);
       ctx.fillStyle = "#1876f2";
 
-    ctx.drawImage(logo, 0, 0, 1280, 1280);
+    ctx.drawImage(logo, 0, 0, 1748, 2480); //last size: 1280x1280
 
     //ctx.font = '34pt Moul'; 
     //ctx.fillStyle = "#fe4f27";
@@ -44,15 +44,15 @@ function prev(){
 
     function drawStroked(text, x, y) {
         ctx.textAlign = "center"; 
-        ctx.font = '44px Moul';
+        ctx.font = '54px Moul';
         ctx.strokeStyle = 'white';
         ctx.lineWidth = 6;
         ctx.strokeText(text, x, y);
-        ctx.fillStyle = "#fe4f27";
+        ctx.fillStyle = "#067606";
         ctx.fillText(text, x, y);
     }
 
-    drawStroked(name.innerHTML, 640, 670);
+    drawStroked(name.innerHTML, 874, 1250); //last value 640, 670
 
 
 
