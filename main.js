1366482736c61a71a7742742310e1e373b23aad8
Kakratana <39192742+Kakratana@users.noreply.github.com>
2025-01-21 08:04:52 +0700

    1

--- main.js
+++ main.js
@@ -10,57 +10,21 @@ const image = document.getElementById("imgDisplayed");
 const logo = document.getElementById("logo");
 
 function prev(){
-	  ctx.font = "14px Moul";
-      var x = document.getElementById("form");
-      x.style.display = "none";
+    var x = document.getElementById("form");
+    x.style.display = "none";
 
-      var xz = document.getElementById("show");
-      xz.style.display = "block";
+    var xz = document.getElementById("show");
+    xz.style.display = "block";
+    let inName = document.getElementById("inName").value;
+    const name = document.getElementById("name");
+    name.innerHTML = inName;
 
+    ctx.drawImage(logo, 0, 0, 1280, 1280);
 
-      let inName = document.getElementById("inName").value;
-
-
-      const name = document.getElementById("name");
-
-
-
-      name.innerHTML = inName;
-
-      
-     // ctx.fillStyle = "#ffffff";
-     // ctx.fillRect(0, 0, 800, 900);
-      //ctx.fillStyle = "#1876f2";
-     // ctx.fillRect(0, 0, 800, 100);
-      // ctx.fillStyle = "#f1f3f4";
-      // ctx.fillRect(0, 750, 800, 50);
-      //ctx.fillStyle = "#1876f2";
-
-
-    ctx.drawImage(logo, 0, 0, 1280, 1280); //last size: 1280x1280
-
-    ctx.font = '32pt Moul'; 
+    ctx.font = "32px 'Moul'"; 
     ctx.fillStyle = "#fff200";
     ctx.textAlign = "center";   
     ctx.fillText(name.innerHTML, 640, 670);
-
-    //function drawStroked(text, x, y) {
-       // ctx.textAlign = "center"; 
-       // ctx.font = '42px Moul';
-      //  ctx.strokeStyle = 'white';
-       // ctx.lineWidth = 0;
-    //    ctx.strokeText(text, x, y);
-      //  ctx.fillStyle = "#e8e614";
-      //  ctx.fillText(text, x, y);
-  //  }
-
-    //drawStroked(name.innerHTML, 518.5, 660); //last value 640, 670
-
-
-
-      
-
-
 }
 
 const download = document.getElementById("down");
